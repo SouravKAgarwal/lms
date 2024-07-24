6e5d1400d525ca904081f56041482f7d8d6740b2
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2024-07-24 19:44:52 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -10,14 +10,16 @@ export const accessTokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire *  60 * 1000),
   maxAge: accessTokenExpire *  60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
+  secure:"true",
 };
 
 export const refreshTokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "none",
+  secure:"true",
 };
 
 export const sendToken = (user, statusCode, res) => {
@@ -25,11 +27,6 @@ export const sendToken = (user, statusCode, res) => {
   const refreshToken = user.signRefreshToken();
 
   redis.set(user._id, JSON.stringify(user));
-
-  if (process.env.NODE_ENV === "production") {
-    accessTokenOptions.secure = true;
-    refreshTokenOptions.secure = true;
-  }
 
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
